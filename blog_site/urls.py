06aab889a21638a5ac763319cd99afda8b4ccc70
diasjakupov <dias.dzhakupov.68@mail.ru>
2020-06-14 15:16:56 +0600

    create detail page

--- blog_site/urls.py
+++ blog_site/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.articles, name='mainpage'),
     path('<int:cat_pk>', views.category, name='category'),
+    path('<str:news_t>', views.detailPage, name='detail'),
 ]
--- blog_site/views.py
+++ blog_site/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, HttpResponse
-from .models import *
+from .models import News, Category
+from django.shortcuts import get_object_or_404
 
 
 def articles(request):
@@ -12,6 +13,12 @@ def category(request, cat_pk):
     all_cat=Category.objects.all()
     selected_news=current_cat.news_set.all()
     return render(request, 'blog/article.html', {'news':selected_news, 'all_cat':all_cat})
+
+def detailPage(request, news_t):
+    current_art = get_object_or_404(News, title=news_t)
+    current_art.views=current_art.views + 1
+    current_art.save()
+    return render(request, 'blog/detail.html', {'article': current_art})
     
 
 
