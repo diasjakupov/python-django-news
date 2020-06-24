88eeff3a37fb47864d87a62ef3e344a6ae43c0e1
diasjakupov <dias.dzhakupov.68@mail.ru>
2020-06-24 17:13:56 +0600

    pagination

--- blog_site/views.py
+++ blog_site/views.py
@@ -3,20 +3,33 @@ from .models import News, Category
 from django.shortcuts import get_object_or_404
 from django.db.models import F
 from .forms import NewsForm
+from django.core.paginator import Paginator
 
 
 def articles(request):
-    article=News.objects.all()
+    article=News.objects.filter(is_published=True)
+
+    paginator = Paginator(article, 9)
+    page_number = request.GET.get('page', 1)
+    page_obj = paginator.get_page(page_number)
+
+
+
     all_cat=Category.objects.all()
     queryset=News.objects.select_related('Category')
-    return render(request, 'blog/article.html', {'news':article, 'all_cat':all_cat})
+    return render(request, 'blog/article.html', {'news':article, 'all_cat':all_cat, 'page_obj': page_obj})
 
 def category(request, cat_t):
     current_cat=get_object_or_404(Category, title=cat_t)
     all_cat=Category.objects.all()
     selected_news=current_cat.news_set.all()
+
+    paginator = Paginator(selected_news, 9)
+    page_number = request.GET.get('page', 1)
+    page_obj = paginator.get_page(page_number)
+
     queryset=News.objects.select_related('Category')
-    return render(request, 'blog/article.html', {'news':selected_news, 'all_cat':all_cat})
+    return render(request, 'blog/article.html', {'news':selected_news, 'all_cat':all_cat, 'page_obj': page_obj})
 
 def detailPage(request,news_pk):
     current_art = get_object_or_404(News, pk=news_pk)
